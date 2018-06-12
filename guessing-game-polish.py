22e8c6c5de939c90b7c84cc392535dbe49c6d023
Jakub <39642297+kbjay01@users.noreply.github.com>
2018-06-12 18:37:17 +0200

    Fixed bug
    Naprawiono błąd, który po wybraniu niedozwolnego poziomu trudnośći, program kontynuował pracę, zamiast ją zakończyć

--- guessing-game-polish.py
+++ guessing-game-polish.py
@@ -1,10 +1,11 @@
 from random import randint
 import time
  
-#Coded by KBJay')
-ls_poz = [1,2,3,4]           # Lista poziomow
+print('[i] Coded by KBJay')
+print('----------')
+ls_poz = [1,2,3,4]           #lista poziomow
 def menu():
-    print('***MENU***')      # Menu poziomow
+    print('***MENU***')
     print('----------')
     print('Wybierz poziom trudnosci:')
     print('1. Latwy')
@@ -17,51 +18,42 @@ print('')
 wybor = input('Wybieram: ')
 try:
     wybor = int(wybor)
-except ValueError:         							# Sprawdzenie czy wprowadzony poziom trudnosci jest intem
+except ValueError:           #Sprawdzenie czy wybor jest intem
     print('----------')
     print('[!] Blad. Podaj liczbe z zakresu [1-4]')
    
-def Proby():              							
-# Funkcja odpowiadajaca za zgadywanie
-    x = 1                  							
-# X utworzony tylko po to, aby miec warunek w petli while
+def Proby():
+    x = 1
     il_los = 0
     while x > 0:
-        los = input('Zgaduje ze to: ')   			
-# Uzytkownik zgaduje liczbe
-        il_los = int(il_los) +1 
-# Licznik, pod koniec dzieki niemu, uzytkownik wie za ktorym razem trafil w wygrana liczbe
+        los = input('Zgaduje ze to: ')
+        il_los = int(il_los) +1
         print('----------')
        
         try:
             los = int(los)
-# Weryfikacja czy liczba ktora uzytkownik podaje, jako prawdopodobna jest intem
  
         except ValueError:
             print('[!] Blad. Podaj liczbe z okreslonego wczesniej zakresu')
             print('')
             continue
-# Jesli uzytkownik jako prawdopodobna liczbe przykladowo tekst
        
         if los < wynik:
             print('Za malo')
             print('')
             continue
-# Jesli prawdopodbna liczba zdefiniowana przez uzytkownika jest mniejsza od tej wygranej
            
         if los > wynik:
             print('Za duzo')
             print('')
-            continue          
-# Jesli prawdopodbna liczba zdefiniowana przez uzytkownika jest wieksza od tej wygranej  
- 
+            continue
+           
         if los == wynik and wybor != 4:
             print('Gratulacje udalo Ci sie! Byla to twoja ' + str(il_los) + ' proba. Sproboj jeszcze raz z trudniejszym poziomem')
-# Jesli prawdopodbna liczba zdefiniowana przez uzytkownika jest liczba wygrana: 
-         
+           
         if los == wynik and wybor == 4:
             print('Wow. Jestes niesamowity, gratulacje, byla to twoja ' + str(il_los) + ' proba!')
-# Jesli prawdopodbna liczba zdefiniowana przez uzytkownika jest liczba wygrana, a poziom trudnosci jest ustawiony jako 'Koszmarny' 
+           
         time.sleep(2)
         print('')
         exit()
@@ -88,5 +80,6 @@ if wybor == 4:
    
 if wybor not in ls_poz:
     print('[!] Blad. Podano niedozwolony poziom trudnosci. Wybierz poziom z zakresu [1-4]')
+    exit()
  
 Proby()
